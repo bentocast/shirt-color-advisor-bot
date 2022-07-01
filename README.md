68e0142f7784b279b1a3b507f8bf6f217bdf026b
Chatchavit Nitipongpun <chatchavit.nitipongpun@agoda.com>
2022-07-01 23:47:21 +0700

    Update source code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# shirt-color-advisor-bot
-Simple bot that broadcast a shirt color suggestion.
+# :shirt: Shirt Color Advisor Bot :shirt
+
+This project is to built a simple bot in `Line` Messaging Platform. It will give an advice which color of your shirt is good for you on that day, in the morning (7:00 AM) everyday.
+
+## :running: How to deploy :running
+
+This bot is running in `Google Cloud Function` on my personal account. This function is just to broadcast a today's picture in `resources` directory to all users, which consists of 2 files
+
+1. `google_cloud_function.py` which is the main function file
+2. `requirements` which is the dependency file
+
+
+
+Once this function is deployed and ready to use. I set up the `Google Cloud Scheduler` to trigger this function at the specific time everyday.
---
+++ google_cloud_function.py
@@ -0,0 +1,39 @@
+import os
+from datetime import datetime
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.models import (
+    ImageSendMessage
+)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+line_bot_api = LineBotApi(channel_access_token)
+
+shirt_color = {
+    0: 'https://github.com/bentocast/shirt-color-advisor-bot/blob/main/resources/Cloth_Monday.jpg?raw=true',
+    1: 'https://github.com/bentocast/shirt-color-advisor-bot/blob/main/resources/Cloth_Tuesday.jpg?raw=true',
+    2: 'https://github.com/bentocast/shirt-color-advisor-bot/blob/main/resources/Cloth_Wednesday.jpg?raw=true',
+    3: 'https://github.com/bentocast/shirt-color-advisor-bot/blob/main/resources/Cloth_Thursday.jpg?raw=true',
+    4: 'https://github.com/bentocast/shirt-color-advisor-bot/blob/main/resources/Cloth_Friday.jpg?raw=true',
+    5: 'https://github.com/bentocast/shirt-color-advisor-bot/blob/main/resources/Cloth_Saturday.jpg?raw=true',
+    6: 'https://github.com/bentocast/shirt-color-advisor-bot/blob/main/resources/Cloth_Sunday.jpg?raw=true',
+}
+def callback(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+
+    weekday = datetime.now().weekday()
+    image_message = ImageSendMessage(
+        original_content_url=shirt_color[weekday],
+        preview_image_url=shirt_color[weekday]
+    )
+    line_bot_api.broadcast(image_message)
+    return 'OK'
